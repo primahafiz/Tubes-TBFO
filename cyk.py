04742fd5cf10da1d795e4ad205545c39e3fee93f
Ahmad Romy <ahmadromyzahran@gmail.com>
2021-11-22 18:44:03 +0700

    add cyk, main, txt

---
+++ cyk.py
@@ -0,0 +1,44 @@
+import re
+
+def LoadCNF(cnfPath): # mengembalikan dictionary {Body : [head1, head2, ..]}
+    chomskyDict = {}
+    f = open(cnfPath,'r')
+    f_contents = f.readline()
+    while(len(f_contents)>0):
+        f_contents = f_contents.strip()
+        head, bodies = f_contents.split(' -> ')
+        bodies = bodies.replace(' ', '') # yang ruas kanannya 2 varaibel bkl jd VAR1VAR2, kalo ga ambigu gpp
+        bodies = bodies.split('|')
+        for body in bodies:
+            if(chomskyDict.get(body)==None):
+                chomskyDict.update({body: [head]})
+            else:
+                chomskyDict[body].append(head)
+        f_contents = f.readline()
+    f.close()
+
+    return chomskyDict
+
+def cyk(tokens, chomskyDict):
+    n = len(tokens)
+    print(n)
+    cykTable = [[[] for j in range(n-i)] for i in range(n)]
+
+    #inisialisasi cykTable[0] berisi variable dengan ruas kanan rule terminal i
+    for i in range(n):
+        if(chomskyDict.get(tokens[i])!=None):
+            cykTable[0][i] = chomskyDict[tokens[i]]
+    
+    for i in range(1,n): # tinggi i+1
+        for j in range(n-i):
+            for k in range(i): # tinggi nya k+1 dan i-k
+                # string[j..j+i], dibentuk dari string[j..j+k] dan string[j+k+1..j+i]
+                for var1 in cykTable[k][j]:
+                    for var2 in cykTable[i-k-1][j+k+1]:
+                        if(chomskyDict.get(var1+var2)!=None):
+                            cykTable[i][j] += chomskyDict[var1+var2]
+    
+    return cykTable
+
+def verdict(cykTable):
+    return ('S0' in cykTable[-1][0])
---
+++ main.py
@@ -0,0 +1,29 @@
+import lexer
+import cyk
+
+# Load Chomsky Normal Form
+chomskyDict = cyk.LoadCNF("cnf.txt")
+
+print(chomskyDict)
+print(len(chomskyDict))
+
+# Tokenize
+tokens = lexer.main_parser("input3.txt")
+
+print(tokens)
+print(len(tokens))
+print("abis tokenized")
+
+# CYK
+cykTable = cyk.cyk(tokens, chomskyDict)
+
+for row in cykTable:
+    print(row)
+
+if(cyk.verdict(cykTable)):
+    print("Accepted")
+else:
+    print("Syntax Error")
+
+
+
