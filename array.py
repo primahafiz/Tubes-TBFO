e22ad96e0a7925df244a55adea488e607904952a
01 494 Aji <ajiandhika05@gmail.com>
2021-11-24 20:53:05 +0700

    CFG

--- array.py
+++ array.py
@@ -1,5 +1,5 @@
-w = []
-x = [0 for i in range(2)]
-y = [x for j in range(20)]
-z = [['z' for a in range(3)] for b in range(5)]
-v = [[] for c in x]
+class animal:
+    def __init__(self, species, name, age):
+        self.species = species
+        self.name = name
+        self.age = age
