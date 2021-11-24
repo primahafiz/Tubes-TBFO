e0bf29070d3859d7d42fd356954b08eebfa961fc
Primesz <primahafiz@gmail.com>
2021-11-24 10:35:19 +0700

    tambahin nfa dan benerin lexer

--- lexer.py
+++ lexer.py
@@ -1,44 +1,9 @@
 import re
-import sys
 
-terminal=[None, None, "True", "False", "class", "is", "return", "None", "continue", "pass", "break", "for", "def", "from", "import", "while", "and", "or", "not", "with", "as", "if", "elif", "else", "range", "print", "open", "in", "raise", "variable", "comment", "comment", "string", "string", "number", "newline", "+", "-", "*", "/", "=", "(", ")", ">", "<", "%", ":", ",", ".", "[", "]", "&", "|", "^", "~", "!"]
-
-tokenExprs = [
-    terminal,
-    [
-        r'[ \t]+',
-        r'#[^\n]*',
-        r"\bTrue\b", r"\bFalse\b", 
-        r"\bclass\b", r"\bis\b", r"\breturn\b", r"\bNone\b", r"\bcontinue\b", r"\bpass\b", r"\bbreak\b", r"\bfor\b", r"\bdef\b", r"\bfrom\b", r"\bimport\b", r"\bwhile\b", r"\band\b", r"\bor\b", r"\bnot\b", r"\bwith\b", r"\bas\b", r"\bif\b", r"\belif\b", r"\belse\b", r"\brange\b", r"\bprint\b", r"\bopen\b", r"\bin\b", r"\braise\b", 
-        r"[a-zA-Z_][a-zA-Z0-9_]*",
-        r'\'\'\'[(?!(\'\'\'))\w\W]*\'\'\'',
-        r'\"\"\"[(?!(\"\"\"))\w\W]*\"\"\"',
-        r'\"[^\"\n]*\"', 
-        r'\'[^\'\n]*\'',
-        r"[0-9]+",
-        r'\n', 
-        r'\+', 
-        r"\-", 
-        r"\*", 
-        r"/", 
-        r"=", 
-        r"\(", 
-        r"\)", 
-        r">", 
-        r"<", 
-        r"%", 
-        r"\:", 
-        r",", 
-        r"\.",
-        r"\[",
-        r"\]",
-        r'&',
-        r'\|',
-        r"\^",
-        r"~",
-        r'!'
-    ]
-]
+terminal=["True", "False", "variable", "class", "is", "return", "None", "continue", "pass", "break", "for", "def", "from", "import", "while", "and", "or", "not", "with", "as", "if", "elif", "else", "range", "print", "open", "in", "raise", "space", "string", "number", "+", "-", "*", "/", "=", "(", ")", ">", "<", "%", ":", "'", '"', ",", ".", "[", "]", "&", "|", "^", "~", "!"]
+lowercase=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+uppercase=['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T,','U','V','W','X','Y','Z']
+number=['1','2','3','4','5','6','7','8','9','0']
 
 def conditional_validation(lines):
     conditions=[]
@@ -57,31 +22,120 @@ def conditional_validation(lines):
             return False
     return True
 
+def is_variable(arr,startpos,endpos):
+    if arr[startpos] in terminal:
+        return False
+    else:
+        if(arr[startpos][0] in lowercase or arr[startpos][0] in uppercase or arr[startpos][0] == '_'):
+            for i in range(1,len(arr[startpos])):
+                if arr[startpos][i] in lowercase or arr[startpos][i] in uppercase or arr[startpos][i] == '_' or arr[startpos][i] in number:
+                    continue
+                else:
+                    return False
+            return True
+        else:
+            return False
+
+def is_number(arr,startpos,endpos):
+    for i in range(len(arr[startpos][0])):
+        if arr[startpos][i] in number:
+            continue
+        else:
+            return False
+    return True
+
+def is_string(arr,startpos,endpos):
+    if arr[startpos] == "'":
+        pos=0
+        for i in range(startpos+1,endpos):
+            if arr[i] == "'":
+                pos=i
+                return pos
+    if arr[startpos] == '"':
+        pos=0
+        for i in range(startpos+1,endpos):
+            if arr[i] == '"':
+                pos=i
+                return pos
+        return pos
+    else:
+        return 0
+
+def is_comment(arr,startpos,endpos):
+    if arr[startpos] == '#':
+        pos=endpos-1
+        for i in range(startpos+1,endpos):
+            if(arr[i]=='\n'):
+                pos=i
+                return pos
+        return pos
+    else:
+        return 0
+
+def is_multilinecomment(arr,startpos,endpos):
+    if endpos-startpos > 2:
+        if(arr[startpos]=='"' and arr[startpos+1]=='"' and arr[startpos+2]=='"'):
+            print(1)
+            state=0
+            for i in range(startpos+3,endpos):
+                if arr[i] == '"':
+                    state+=1
+                else:
+                    state=0
+                if(state==3):
+                    return i
+    if endpos-startpos > 2:
+        if(arr[startpos]=="'" and arr[startpos+1]=="'" and arr[startpos+2]=="'"):
+            state=0
+            for i in range(startpos+3,endpos):
+                if arr[i] == "'":
+                    state+=1
+                else:
+                    state=0
+                if(state==3):
+                    return i
+        return 0
+    else:
+        return 0
+    
+def is_newline(arr,startpos,endpos):
+    return arr[startpos]=='\n'
+
 def main_parser(file):
     f=open(file,'r')
     read_file=f.read()
     f.close()
-    # ops=[r'\+','-',r'\*',r'\\','<','>','=','%',r'\(',r'\)',':','\n',r'\.',',',r'\[',r'\]']
-    tokens=[]
-    pos = 0
-    while(pos<len(read_file)):
-        match = None
-        for i in range(len(tokenExprs[0])):
-            pattern = tokenExprs[1][i]
-            tag = tokenExprs[0][i]
-            regex = re.compile(pattern)
-            match = regex.match(read_file, pos)
-            if match:
-                if tag:
-                    tokens.append(tag)
-                break
-        if not match:
-            print(tokens)
-            print("Syntax Error")
-            sys.exit(1)
-        else:
-            pos = match.end(0)
-    return tokens
-
-# file="input.txt"
-# main_parser(file)
+    ops=[r'\+','-',r'\*',r'\\','<','>','=','%',r'\'',r'\"',r'\(',r'\)',':','\n',r'\.',',',r'\[',r'\]',r"\'\'\'",r'\"\"\"', r"\[", r"\]", r"\&", "\|", "\^", "~", "\!"]
+    temp=[]
+    results=re.split(" +",read_file)
+    lines=results
+    for op in ops:
+        for content in lines:
+            formats=r"("+op+r")"
+            split_res=re.split(formats,content)
+            for split_string in split_res:
+                if(split_string!=''):
+                    temp.append(split_string)
+        lines=temp
+        temp=[]
+    print(lines)
+    pos=0
+    while(pos<len(lines)):
+        loc_comment=is_comment(lines,pos,len(lines))
+        loc_string=is_string(lines,pos,len(lines))
+        loc_multilinecomment=is_multilinecomment(lines,pos,len(lines))
+        if (loc_multilinecomment):
+            lines[pos:loc_multilinecomment+1]=['comment']*1
+        elif (loc_string):
+            lines[pos:loc_string+1]=['string']*1
+        elif (loc_comment):
+            lines[pos:loc_comment+1]=['comment']*1
+        elif (is_variable(lines,pos,len(lines))):
+            lines[pos]='variable'
+        elif (is_number(lines,pos,len(lines))):
+            lines[pos]='number'
+        elif is_newline(lines,pos,len(lines)):
+            lines[pos]='newline'
+        pos+=1
+    print(lines)
+    return lines
